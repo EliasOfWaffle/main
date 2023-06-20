4ec43318c68090552cf2b03c23274bb559d89d7a
EliasOfWaffle <128154816+EliasOfWaffle@users.noreply.github.com>
2023-06-20 16:17:39 -0300

    build: Add rpmfusion nonfree tainted repo
    nouveau-firmware package is from nonfree tainted repo

--- build.sh
+++ build.sh
@@ -19,6 +19,7 @@ fi
 wget -P /tmp/rpms \
     https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-${RELEASE}.noarch.rpm \
     https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-${RELEASE}.noarch.rpm
+    https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-tainted-${RELEASE}.noarch.rpm
 
 rpm-ostree install \
     /tmp/rpms/*.rpm \
